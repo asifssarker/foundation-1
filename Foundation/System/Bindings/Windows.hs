f85abab19f007e5b7e7f24b651e0ee21a8ad9fca
Vincent Hanquez <vincent@snarc.org>
2017-03-06 14:49:56 +0000

    Add more sys dependent handle code

--- Foundation/System/Bindings/Windows.hs
+++ Foundation/System/Bindings/Windows.hs
@@ -1,2 +1,9 @@
 module Foundation.System.Bindings.Windows
      where
+
+type CFd = CInt
+
+foreign import capi unsafe "_wsopen"
+    sysWindowsWsopen :: Ptr CWChar -> CInt -> CInt -> CInt -> IO CInt
+foreign import capi unsafe "_read"
+    sysWindowsRead :: Ptr CFd -> CUInt -> IO CInt
